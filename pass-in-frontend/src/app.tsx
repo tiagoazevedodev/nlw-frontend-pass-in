d12d1370e5b6f69646e294aaac20f0e8490b961c
Tiago Azevedo <tiagogadenz@icloud.com>
2024-04-01 22:32:17 -0300

    Uploadind the initial repo

---
+++ pass-in-frontend/src/app.tsx
@@ -0,0 +1,15 @@
+import { AttendeeList } from "./components/attendee-list";
+import { Header } from "./components/header";
+
+
+export function App() {
+ return (
+    <div>
+      <Header />
+      <AttendeeList />
+    </div>  
+   )
+}
+
+
+
---
+++ pass-in-frontend/src/components/attendee-list.tsx
@@ -0,0 +1,7 @@
+export function AttendeeList() {
+    return (
+        <div>
+            <h1>Participantes</h1>
+        </div>
+    )
+}
